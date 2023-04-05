07a5b8bd92002c6a3733bdd6c8dc6f0707041458
Yongseok <yongseok.dev@gmail.com>
2023-04-05 14:15:34 +0900

    Feat: ejs setting

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-import express from "express";
 import dotenv from "dotenv";
+import express from "express";
+
 dotenv.config();
+const port = process.env.PORT || "3000";
 
 const app = express();
-const port = process.env.PORT || "3000";
+app.set("view engine", "ejs");
+app.set("views", "./src/views");
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  const data = { title: "Hello", message: "Welcome to my app!" };
+  res.render("index", data);
 });
 
 app.listen(port, () => {
