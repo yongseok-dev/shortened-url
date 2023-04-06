8c7314d787c6c1434d0492c9e1fd737cedf533e3
Yongseok <yongseok.dev@gmail.com>
2023-04-06 17:51:52 +0900

    Feat: body-parse set

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import express from "express";
+import bodyParser from "body-parser";
 import router from "./src/middleware/Router.js";
 
 dotenv.config();
@@ -9,6 +10,8 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", "./src/views");
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(router);
 
 app.listen(port, () => {
