8647fa71f2a85de960b7ec06aa720014ba627b72
Yongseok <yongseok.dev@gmail.com>
2023-04-07 08:35:44 +0900

    Feat: url, main 컨트롤러 url 모델 연결

--- src/controller/Main.controller.js
+++ src/controller/Main.controller.js
@@ -1,31 +1,12 @@
+import URLModel from "../model/Url.model.js";
+
 const MainController = {
-  get: (req, res) => {
+  get: async (req, res) => {
     const data = {
       title: "단축URL서비스",
       user: "yongseok",
-      list: [
-        {
-          id: "id",
-          url_short: "url_short",
-          url_long: "url_long",
-          count: "count",
-          code: "code",
-          explanation: "explanation",
-          created_at: "created_at",
-          updated_at: "updated_at",
-        },
-        {
-          id: "id",
-          url_short: "url_short",
-          url_long: "url_long",
-          count: "count",
-          code: "code",
-          explanation: "explanation",
-          created_at: "created_at",
-          updated_at: "updated_at",
-        },
-      ],
     };
+    data["list"] = await URLModel.selectAll(undefined);
     res.render("list", data);
   },
 };
--- src/controller/Url.controller.js
+++ src/controller/Url.controller.js
@@ -1,10 +1,17 @@
+import URLModel from "../model/Url.model.js";
 const URLController = {
   get: (req, res) => {
     const data = { title: "단축URL서비스", message: "단축URL 서비스입니다." };
+    data["short_url"] = req.params.short;
+    console.log(URLModel.select(data));
     res.json(data);
   },
   post: (req, res) => {
+    console.log(req.body);
+    const userid = 1;
     const data = {
+      url_long: req.body.url,
+      explanation: req.body.explanation,
       title: "단축URL서비스",
       list: {
         id: "id",
@@ -17,6 +24,7 @@ const URLController = {
         updated_at: "updated_at",
       },
     };
+    URLModel.insert(data, userid);
     res.json(data);
   },
   delete: (req, res) => {
