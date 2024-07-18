904f05a216e7e519a494f047a5b908fbd7728862
yongseok <106823684+yongseok-dev@users.noreply.github.com>
2024-07-18 18:23:52 +0900

    Update inputValidation.js

--- src/util/inputValidation.js
+++ src/util/inputValidation.js
@@ -1,6 +1,14 @@
 function isValidUrl(url) {
-  const pattern =
-    /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([/\w .-@#_=&]*)*\/?$/;
+  const pattern = new RegExp(
+    "^(https?:\\/\\/)?" + // 프로토콜
+      "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // 도메인 이름
+      "((\\d{1,3}\\.){3}\\d{1,3}))" + // 또는 IP (v4) 주소
+      "(\\:\\d+)?" + // 포트
+      "(\\/[-a-z\\d%@_.~+&:]*)*" + // 경로
+      "(\\?[;&a-z\\d%@_.,~+&:=-]*)?" + // 쿼리 문자열
+      "(\\#[-a-z\\d_]*)?$",
+    "i" // 프래그먼트 식별자
+  );
   return pattern.test(url);
 }
 function escapeHtml(unsafe) {
